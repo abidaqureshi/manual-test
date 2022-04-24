2c3ce3aa46e9851429ea9eba78759fd258cee337
Abid Ali <abid.aqureshi@gmail.com>
2022-04-24 19:01:32 +0100

    structure and scss updated

---
+++ src/api/Questionnaire.js
@@ -0,0 +1,5 @@
+import {API_RESPONSE} from '../constants/globla';
+
+export const getQuestions = () => {
+    return API_RESPONSE;
+}
--- src/components/questionnaire/Questionaire.jsx
+++ src/components/questionnaire/Questionaire.jsx
@@ -1,6 +1,15 @@
+import {useEffect, useState} from "react";
 import { Modal } from 'antd';
 import '../../style/scss/questionnaire.scss';
+import {getQuestions} from '../../api/Questionnaire';
+
+
 const Questionairre = ({visible, handleCancel, handleOk}) => {
+
+    useEffect(()=>{
+        const questions = getQuestions();
+        console.log(questions);
+    },[visible]);
 
     return (
         <Modal
